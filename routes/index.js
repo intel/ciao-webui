bb234ec5eb34a6fc814a50af93e37c1176569def
Jorge A Villalobos <jorge.villalobos.gutierrez@intel.com>
2016-04-14 16:10:17 -0500

    Redirect from root to login page
    Entering '/' will redirecto to '/authenticate/login'
    
    Signed-off-by: Jorge A Villalobos <jorge.villalobos.gutierrez@intel.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CIAO UI ', urls: global.urls_config });
+    res.redirect('/authenticate/login');
 });
 
 module.exports = router;
