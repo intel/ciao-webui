c179589f4b351aa34004fbe61d5d9bd14ba573e0
Arturo Nunez <arturo.nunez@intel.com>
2016-08-09 15:28:25 -0500

    Fixes for Makefile and removal of test code.
    - Fixes #132, install.sh includes temporal testing code
    - Fixes #133, Makefile - install-dev does not build tenant-details
    - Fixes #134, Makefile - clean does not remove build/javascripts/ files
    
    Signed-off-by: Arturo Nunez <arturo.nunez@intel.com>

--- install.sh
+++ install.sh
@@ -41,23 +41,6 @@ cp -r css/css_framework/* build/stylesheets
 cp css/d3_components/d3Framework.css build/stylesheets
 cp -r tempDownloads/fonts public/stylesheets/
 
-# ---------------------------------------  Temporal jquery-ui for testing -----------------------------------------------------
-
-# Create javascript directory
-mkdir public/javascripts
-# Create library working directory
-mkdir public/javascripts/library
-
-# get vendor libraries
-# Copy all the js libraries to the public Path
-if [ ! -f public/javascripts/library/jquery-ui.js ]; then
-    curl  https://code.jquery.com/ui/1.11.4/jquery-ui.min.js -o jquery-ui.min.js
-    mv jquery-ui.min.js public/javascripts/library/jquery-ui.js
-fi
-
-# ---------------------------------------  End Temporal jquery-ui for testing -------------------------------------------------
-
-
 # removing temporal files
 rm -rf tempDownloads
 
@@ -83,8 +66,7 @@ bootstrap="$cssStart/bootstrap/dist/css/bootstrap.min.css$cssEnd"
 framework="$cssStart/stylesheets/framework.css$cssEnd"
 d3="$cssStart/stylesheets/d3Framework.css$cssEnd"
 jquery="$jsStart/jquery/dist/jquery.js$jsEnd"
-#jqueryui="$jsStart/jquery-ui/jquery-ui.js$jsEnd"
-jqueryui="$jsStart/javascripts/library/jquery-ui.js$jsEnd"
+jqueryui="$jsStart/jquery-ui/jquery-ui.js$jsEnd"
 datamanager="$jsStart/javascripts/datamanager.js$jsEnd"
 loadDatamanager="<script type='text/javascript'>window.datamanager.loadData((<%- JSON.stringify(data)%>));</script>"
 validations="$jsStart/util/validations.js$jsEnd"
