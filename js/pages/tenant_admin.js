f6f269f91f64419484778d1212a334d5089ecbad
Teresita de Jesus Guerrero Vela <teresita.guerrero.vela@intel.com>
2016-08-31 10:55:49 -0500

    Tenant - Alert message displayed after launching instances -Fix
    - Disabled logger from add instances component
    
    Signed-off-by: Teresita de Jesus Guerrero Vela <teresita.guerrero.vela@intel.com>

--- js/pages/tenant_admin.js
+++ js/pages/tenant_admin.js
@@ -32,7 +32,7 @@ $('document').ready(function () {
     // Component to Add instances
     datamanager.onDataSourceSet('add-instances', function (sourceData) {
         ReactDOM.render(
-            <AddInstances sourceData={sourceData} logger={logger}/>,
+            <AddInstances sourceData={sourceData}/>,
             document.getElementById("add-instances"));
     });
 
