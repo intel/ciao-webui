a391edd7d8af5f060eb7031296b0ab80f22eedb2
Arturo Nunez <arturo.nunez@intel.com>
2016-09-22 16:47:24 -0500

    Image: implement image creation functionality (API)
        Implements the following endpoint:
        - Create image - POST /v2/images
    
    Signed-off-by: Arturo Nunez <arturo.nunez@intel.com>

--- core/imageService.js
+++ core/imageService.js
@@ -29,6 +29,22 @@ imageService.prototype.getImageDetails = function () {
     };
 };
 
+imageService.prototype.createImage = function () {
+    var adapter = this.adapter;
+    var tokenManager = this.tokenManager;
+    return function (req, res, next) {
+        var uri = "/v2/"+req.params.tenant+"/images";
+
+        var image = req.body.image? req.body :{
+            disk_format:req.body.disk_format,
+            name: req.body.name
+        };
+        return adapter.onSuccess((data) => res.send(data.json))
+            .onError((data) => res.send(data))
+            .post(uri,image,req.session.token);
+    };
+};
+
 imageService.prototype.deleteImage = function () {
     var adapter = this.adapter;
     var tokenManager = this.tokenManager;
