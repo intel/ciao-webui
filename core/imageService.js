221aa502aa024f20c5b9497dd0631cfb583f0cb5
Arturo Nunez <arturo.nunez@intel.com>
2016-09-22 10:23:15 -0500

    Image: implement read image(s) functionlaity (API)
    Implements the following endpoints:
    - Show images- GET /v2/images
    - Show image details - GET /v2/image/{image_id}
    Fixes #208
    
    Signed-off-by: Arturo Nunez <arturo.nunez@intel.com>

--- core/imageService.js
+++ core/imageService.js
@@ -7,4 +7,26 @@ var imageService = function (adapter, tokenManager) {
     this.tokenManager = tokenManager;
 };
 
+imageService.prototype.getImages = function () {
+    var adapter = this.adapter;
+    var tokenManager = this.tokenManager;
+    return function(req, res, next) {
+        var uri = "/v2/"+req.params.tenant+"/images";
+        return adapter.onSuccess((data) => res.send(data.json))
+            .onError((data) => res.send(data))
+            .get(uri,req.session.token);
+    };
+};
+
+imageService.prototype.getImageDetails = function () {
+    var adapter = this.adapter;
+    var tokenManager = this.tokenManager;
+    return function(req, res, next) {
+        var uri = "/v2/"+req.params.tenant+"/images/"+req.params.image_id;
+        return adapter.onSuccess((data) => res.send(data.json))
+            .onError((data) => res.send(data))
+            .get(uri,req.session.token);
+    };
+};
+
 module.exports = imageService;
